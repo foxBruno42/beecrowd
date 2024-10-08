169d883b5d44cad1eafcc6ff946d1c15234dabcf
Bruno Fox <fox.bruno42@gmail.com>
2024-10-08 00:20:40 -0300

    Inicio das matrizes

---
+++ 1181.py
@@ -0,0 +1,51 @@
+"""
+Neste problema você deve ler um número, indicando uma linha da matriz na qual uma operação deve ser realizada, um caractere maiúsculo, indicando a operação que será realizada, e todos os elementos de uma matriz M[12][12]. Em seguida, calcule e mostre a soma ou a média dos elementos que estão na área verde da matriz, conforme for o caso. A imagem abaixo ilustra o caso da entrada do valor 2 para a linha da matriz, demonstrando os elementos que deverão ser considerados na operação.
+
+Entrada
+A primeira linha de entrada contem um número L (0 ≤ L ≤ 11) indicando a linha que será considerada para operação. A segunda linha de entrada contém um único caractere Maiúsculo T ('S' ou 'M'), indicando a operação (Soma ou Média) que deverá ser realizada com os elementos da matriz. Seguem os 144 valores de ponto flutuante que compõem a matriz, sendo que a mesma é preenchida linha por linha, da linha 0 até a linha 11, sempre da esquerda para a direita.
+
+Saída
+Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
+
+linha=int(input())
+op=input()
+matriz=[]
+for i in range(3):
+    linha=[]
+    for j in range(3):
+        linha+=[0]
+    matriz+=[linha]
+        #num=float(input())
+        #matriz[i][j]=num
+
+for i in range(3):
+    linha=[]
+    for j in range(3):
+        num=float(input())
+        matriz[i][j]=num
+        
+print(matriz)
+sum=0
+for k in range(3):
+    sum += matriz[linha][k]
+if(op=='S'):
+    print(sum)
+"""
+line=int(input())
+op=input()
+matriz=[]
+for i in range(12):
+    linha=[]
+    for j in range(12):
+        linha+=[0]
+    matriz+=[linha]
+
+for i in range(12):
+    for j in range(12):
+        num=float(input())
+        matriz[i][j]=num
+        
+if(op=='S'):
+    print(sum(matriz[line]))
+else:
+    print(sum(matriz[line])/12)
